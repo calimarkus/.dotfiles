2c6bbed5fe7e8e9ba77a9fb0c9330054cf9bcea3
Markus Emrich <markus.emrich@gmail.com>
2020-02-14 10:03:39 -0500

    add osx mic fix for steam

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,5 @@ alias findhere='find . -name'
 # Quick way to rebuild the Launch Services database and get rid
 # of duplicates in the Open With submenu.
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+
+alias fixSteamMic='sudo sqlite3 ~/Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceMicrophone','com.valvesoftware.steam',0,1,1,NULL,NULL,NULL,'UNUSED',NULL,0,1551892126);"'
