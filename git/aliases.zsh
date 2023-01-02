25a946d30547c13f606acd54aab6a02a0f1f71e4
Markus Emrich <emrich@apple.com>
2023-01-02 12:20:39 +0100

    tweak gl shortcut

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,8 +8,11 @@ g () {
 }
 compdef g=git
 
-alias gl='git log --oneline --decorate --graph --all'
+alias gl='git log --oneline --graph --branches'
 compdef _git gl=git-log
+
+alias gl2='git log --oneline --graph --all'
+compdef _git gl2=git-log
 
 alias gs='git status'
 compdef _git gs=git-status
