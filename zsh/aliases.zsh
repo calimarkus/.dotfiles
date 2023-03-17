f087ebbbd8dca68883dee3387142334e17c7d680
Markus Emrich <emrich@apple.com>
2023-03-17 16:40:15 +0100

    add openDerivedData

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,8 @@ alias edit='open -e'
 alias ll='ls -halF'
 alias findhere='find . -name'
 
+alias openDerivedData='open ~/Library/Developer/Xcode/DerivedData'
+
 # Quick way to rebuild the Launch Services database and get rid
 # of duplicates in the Open With submenu.
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
