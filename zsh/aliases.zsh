0e1547cf41d3435525c27d34c2f64b4a9f6f8de6
Markus Emrich <emrich@apple.com>
2023-04-11 16:31:07 +0200

    add pip3 alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,8 @@ alias findhere='find . -name'
 
 alias openDerivedData='open ~/Library/Developer/Xcode/DerivedData'
 
+alias pip3='python3 -m pip'
+
 # Quick way to rebuild the Launch Services database and get rid
 # of duplicates in the Open With submenu.
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
